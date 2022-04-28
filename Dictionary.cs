64b79ae95516f36a4a4fb69cf2046cd20a534e25
SokolMaxim <maximsokol2288@gmail.com>
2022-04-28 18:14:17 +0300

    FCommit

---
+++ Dictionary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstraintOfTheGererics
+{
+    class Dictionary<Tkey, TValue>
+    {
+        private readonly MyList<Tkey> key;
+        private readonly MyList<TValue> value;
+        //-------------------------------------------
+
+        public Dictionary()
+        {
+            this.key = new MyList<Tkey>();
+            this.value = new MyList<TValue>();
+        }
+        //--------------------------------------------
+
+        public void AddToDictionary(Tkey key, TValue value)
+        {
+            this.key.Add(key);
+            this.value.Add(value);
+        }
+        //--------------------------------------------
+
+        public string this[int index]
+        {
+            get { return $"{key[index]} + {value[index]}\n"; }
+        }
+        //--------------------------------------------
+
+        public string this[Tkey index]
+        {
+            get 
+            {
+                for(int i = 0; i < this.key.Count; i++)
+                {
+                   return $"Was found by key\t{index.ToString()}\t + The value\t{value[i].ToString()}";
+                }
+                return $"By the key\t{index}\twasn't found the value\n";
+            }
+        }
+        //--------------------------------------------
+
+        public int Count
+        {
+            get { return key.Count; }
+        }
+        //--------------------------------------------
+
+        public override string ToString()
+        {
+            string stroka = string.Empty;
+            for (int i = 0; i < key.Count; i++)
+            {
+                stroka += key[i] + " " + value[i] + "\n";
+            }
+            if (stroka != null)
+                return stroka;
+            return "В словаре нет значений.";
+        }
+        //--------------------------------------------
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConstraintOfTheGererics
+{
+
+    class BaseClass
+    {
+        public int MyIntProperty { get; set; }
+        public string MyStringProperty { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Task 1
+
+
+
+            //ArrayList lst = new ArrayList();
+            //lst.Add(1);
+            //lst.Add(25);
+            //lst.Add("Hi");
+            //lst.Add(new BaseClass());
+
+            //for (int i = 0; i < lst.Count; i++)
+            //{
+            //    Console.WriteLine(lst[i]);
+            //}
+
+
+
+            // Task 2
+
+
+
+            //var collect = new CarCollection<string>();
+
+            //collect.AddCar("Jaguar", new DateTime(2005, 5, 7));
+            //collect.AddCar("Mercedes-Benz", new DateTime(2007, 8, 9));
+            //collect.AddCar("Ferrari", new DateTime(2021, 4, 5));
+            //collect.AddCar("Lamborghini", new DateTime(2022, 4, 6));
+
+            //Console.WriteLine(collect.ToString());
+
+            //Console.WriteLine(collect.Lenght);
+
+            //collect.Clear();
+
+
+
+            // Task 3
+
+
+
+            //var dict = new Dictionary<string, string>();
+
+            //dict.AddToDictionary("spring", "windy");
+            //dict.AddToDictionary("hello", "bye");
+            //dict.AddToDictionary("nice", "bad");
+
+            //Console.WriteLine(dict.ToString());
+
+            //string str = "hello";
+            //Console.WriteLine(dict[str]);
+
+
+            // Task 4
+
+
+            //var ls = new MyList<int>();
+
+            //ls.Add(1);
+            //ls.Add(2);
+            //ls.Add(3);
+            //ls.Add(4);
+            //ls.Add(5);
+
+            //Console.WriteLine(ls.ToString());
+
+            //Console.WriteLine(ls.Contains(6));
+
+            //Console.WriteLine(ls.Count);
+
+            //for(int i = 0; i < ls.Count; i++)
+            //{
+            //    Console.WriteLine(ls[i]);
+            //}
+        }
+    }
+}
